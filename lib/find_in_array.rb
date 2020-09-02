648686a245a94b94c1b6a34a31ed26f719fd404c
declarative-linter-0823 <grant.shannon90@gmail.com>
2020-09-02 22:31:04 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,8 @@
 def find_element_index(array, value_to_find)
-  array.include?(value_to_find)
+  count = 0
+
+  while count < array.length do
+    puts array == value_to_find
+    count +=1
+  end
 end
